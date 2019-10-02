3ab5055174e106aa066ad8e36842e545dca1235c
up201806680 <up201806680@0d5ac13f-7f68-4838-9180-cc2c738ee2d4>
2019-10-02 11:18:10 +0000

    util_sys_inb() fixed
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t4g03@14 0d5ac13f-7f68-4838-9180-cc2c738ee2d4

--- lab2/utils.c
+++ lab2/utils.c
@@ -17,13 +17,13 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  uint32_t b;
+  uint32_t *b = 0;
 
   if (sys_inb(port, b) != 0) {
     return 1;
   }
 
-  *value = (uint8_t) b;
+  *value = (uint8_t) *b;
 
   return 0;
 }
