d553868d924d4e5ea502c280f2fa3718c23d1293
up201806680 <up201806680@0d5ac13f-7f68-4838-9180-cc2c738ee2d4>
2019-10-02 12:07:47 +0000

    timer_display_conf() implemented
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t4g03@15 0d5ac13f-7f68-4838-9180-cc2c738ee2d4

--- lab2/timer.c
+++ lab2/timer.c
@@ -45,8 +45,73 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
 
-  return 1;
+  union timer_status_field_val value;
+  
+  switch(field) {
+    case 0:
+      value.byte = st;
+      break;
+    case 1:
+      st = st << 2;
+      st = st >> 6;
+      switch (st) {
+        case 0:
+          value.in_mode = INVAL_val;
+          break;
+        case 1:
+          value.in_mode = LSB_only;
+          break;
+        case 2:
+          value.in_mode = MSB_only;
+          break;
+        case 3:
+          value.in_mode = MSB_after_LSB;
+          break;
+        default:
+          return 1;
+      }
+      break;
+    case 2:
+      st = st << 4;
+      st = st >> 5;
+      switch (st) {
+        case 0:
+          value.count_mode = 0;
+          break;
+        case 1:
+          value.count_mode = 1;
+          break;
+        case 2: case 6:
+          value.count_mode = 2;
+          break;
+        case 3: case 7:
+          value.count_mode = 3;
+          break;
+        case 4:
+          value.count_mode = 4;
+          break;
+        case 5:
+          value.count_mode = 5;
+          break;
+        default:
+          return 1;
+      }
+      break;
+    case 3:
+      if (st % 2 == 0) {
+        value.bcd = false;
+      } else {
+        value.bcd = true;
+      }
+      break;
+
+    default: 
+      return 1;
+
+  }
+  timer_print_config(timer, field, value);
+
+  return 0;
 }
