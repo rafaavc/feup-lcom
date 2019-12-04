ff62f701806c32ed5d508ecc0a0c98173fbac844
up201806134 <up201806134@0d5ac13f-7f68-4838-9180-cc2c738ee2d4>
2019-12-04 11:42:34 +0000

    player continued
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t4g03@129 0d5ac13f-7f68-4838-9180-cc2c738ee2d4

--- proj/player.c
+++ proj/player.c
@@ -14,5 +14,36 @@
 extern unsigned grid_height;
 extern unsigned grid_width;
 
+Player * create_player(unsigned x, unsigned y, xpm_image_t * animation_idle[12]){
+    Player *p = (Player*) malloc(sizeof(*p));
+    (*p).x = x;
+    (*p).y = y;
+    (*p).animation_frame = 0;
+    (*p).xvel = 0;
+    (*p).yvel = 0;
+    for (int i = 0; i < 12; i++){
+        (*p).animation_idle[i] = animation_idle[i];
+    }
+    return p;
+}
 
+int p_get_xpos(Player * p) { return (*p).x; }
+int p_get_ypos(Player * p) { return (*p).y; }
+int p_set_xvel(Player * p) { return (*p).xvel; }
+int p_set_yvel(Player * p) { return (*p).yvel; }
+
+unsigned next_animation_frame(Player *p) {
+  (*p).animation_frame++;
+  if ((*p).animation_frame > 12){
+    (*p).animation_frame = 0;
+  }
+  return (*p).animation_frame;
+}
+
+void draw_player(Player *p){
+  unsigned frame = next_animation_frame(p);
+  (*p).x += (*p).xvel;
+  (*p).y += (*p).yvel;
+  draw_pixmap(*(*p).animation_idle[frame],(*p).x,(*p).y,false,PREDEF_COLOR,"");
+}
 
--- proj/player.h
+++ proj/player.h
@@ -5,3 +5,11 @@ typedef struct {
     unsigned animation_frame;
     xpm_image_t * animation_idle[];
 } Player;
+
+Player * create_player(unsigned x, unsigned y, xpm_image_t * animation_idle[12]);
+int p_get_xpos(Player * p);
+int p_get_ypos(Player * p);
+int p_set_xvel(Player * p);
+int p_set_yvel(Player * p);
+unsigned next_animation_frame(Player *p);
+void draw_player(Player *p);
